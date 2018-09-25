2156e5442e71fddd90469d5db1c0cead65e14553
Marty Glaubitz <marty.glaubitz@nexum.de>
2018-09-25 12:45:19 +0200

    fix: properly delete node out of graph

--- pillowpile/graph.py
+++ pillowpile/graph.py
@@ -1,16 +1,22 @@
-def get_document_entry_for_path(graph_json: dict, path_components: list):
+def get_document_entry_for_path(graph_json: dict, path_components: list, return_copys=True):
     last_path_component = path_components[-1]
     parent_doc = graph_json
     result = None
 
+    def transform_result(result_obj: dict):
+        if return_copys:
+            return dict(result_obj)
+        
+        return result_obj
+
     path_components_index = 0
     for path_component in path_components:
         if not path_component in parent_doc:
-            return None, dict(parent_doc), path_components[path_components_index:]
+            return None, transform_result(parent_doc), path_components[path_components_index:]
 
         result = parent_doc[path_component]
         if path_component == last_path_component:
-            return dict(result), dict(parent_doc), None
+            return transform_result(result), transform_result(parent_doc), None
 
         path_components_index += 1
         parent_doc = result
@@ -27,7 +33,7 @@ def create_doc_node_in_graph(graph_json: dict, path_components: list, new_doc_uu
     new_graph_node['id'] = new_doc_uuid
 
 def delete_doc_node_from_graph(graph_json: dict, path_components: list) -> bool:
-    doc_ref, parent_doc, _ = get_document_entry_for_path(graph_json, path_components)
+    doc_ref, parent_doc, _ = get_document_entry_for_path(graph_json, path_components, return_copys=False)
     if not doc_ref:
         return False
 
